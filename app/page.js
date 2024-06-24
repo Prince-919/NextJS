8d748a32ff8d27579f86ffa1a4bf71a23e1d5f5c
Prince Sharma <prince.sharma_cs20@gla.ac.in>
2024-06-24 23:55:18 +0530

    setup meal app and add images

--- app/page.js
+++ app/page.js
@@ -1,12 +1,7 @@
-import Link from "next/link";
-import Header from "@/components/header";
-
 const Home = () => {
   return (
     <main>
       <div>Home</div>
-      <Header />
-      <Link href={"/about"}>About Us</Link>
     </main>
   );
 };
